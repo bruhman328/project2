dd23e3d969c3ce0b1737d0ae035cf908ebf9e33b
bruhman328 <nkassa4015@gmail.com>
2024-11-15 16:38:23 -0500

    Update frogger.py

--- frogger.py
+++ frogger.py
@@ -16,27 +16,35 @@ def select_game_file():
             [2]  	game2.frog
             [3]  	game3.frog
          """)
-    file_selection = int(input('Enter an option or filename: '))
+    file_selection = input('Enter an option or filename: ')
     board = [[], [], []]
-    if file_selection == 1 or file_selection == 'game1.frog':
+    if file_selection == '1' or file_selection == 'game1.frog':
         with open('game1.frog', 'r') as lines:
             board_lines = lines.readlines()
-        for line in board_lines[0]:
-            for lining in line.strip():
-                if lining == 15:
-                    board[0].append(15)
-                else:
-                    board[0].append(int(lining))
-    elif file_selection == 2 or file_selection == 'game2.frog':
+        for line in board_lines[0].strip(''):
+            board[0].append(int(line))
+        for line in board_lines[1].strip(''):
+            board[1].append(int(line))
+        for line in board_lines[2].strip():
+            board[2].append(line)
+    elif file_selection == '2' or file_selection == 'game2.frog':
         with open('game2.frog', 'r') as lines:
             board_lines = lines.readlines()
-        for line in board_lines:
-            board.append(line.strip())
-    elif file_selection == 3 or file_selection == 'game3.frog':
+        for line in board_lines[0].strip(''):
+            board[0].append(int(line))
+        for line in board_lines[1].strip(''):
+            board[1].append(int(line))
+        for line in board_lines[2].strip():
+            board[2].append(line)
+    elif file_selection == '3' or file_selection == 'game3.frog':
         with open('game3.frog', 'r') as lines:
             board_lines = lines.readlines()
-        for line in board_lines:
-            board.append(line.strip())
+        for line in board_lines[0].strip(''):
+            board[0].append(int(line))
+        for line in board_lines[1].strip(''):
+            board[1].append(int(line))
+        for line in board_lines[2].strip():
+            board[2].append(line)
     else:
         print("Not a selection")
         return None
