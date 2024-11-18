6e659714047529d998580383966c92128f497afc
bruhman328 <nkassa4015@gmail.com>
2024-11-18 00:22:54 -0500

    Update frogger.py

--- frogger.py
+++ frogger.py
@@ -9,7 +9,16 @@ RIGHT = 'D'
 JUMP = 'J'
 FROG = '\U0001318F'
 
+def frogger_game(file):
+    for i in range(file[0][1]):
+        file[2].append(' ')
+    file[2][len(file[2]) // 2] = FROG
+    for i in file[0:2]:
+        print(i)
+    for i in file[2:]:
+        print(i)
 
+     
 def select_game_file():
     print("""
             [1]  	game1.frog
@@ -17,41 +26,69 @@ def select_game_file():
             [3]  	game3.frog
          """)
     file_selection = input('Enter an option or filename: ')
-    board = [[], [], []]
+    board = []
     if file_selection == '1' or file_selection == 'game1.frog':
-        with open('game1.frog', 'r') as lines:
-            board_lines = lines.readlines()
-        for line in board_lines[0].strip(''):
-            board[0].append(int(line))
-        for line in board_lines[1].strip(''):
-            board[1].append(int(line))
-        for line in board_lines[2].strip():
-            board[2].append(line)
+        with open('game1.frog', 'r') as file:
+            board_lines = file.readlines()
+            line = board_lines[0].split()
+            lining = list(int(num) for num in line)
+            board.append(lining)
+
+            line1 = board_lines[1].split()
+            lining1 = list(int(num) for num in line1)
+            board.append(lining1)
+            board.append([])
+
+            new_row = []
+            for i in range(2, len(board_lines)):  
+                row = list(board_lines[i].strip())
+                new_row.append(row)
+
+            board.append(new_row)
+
     elif file_selection == '2' or file_selection == 'game2.frog':
-        with open('game2.frog', 'r') as lines:
-            board_lines = lines.readlines()
-        for line in board_lines[0].strip(''):
-            board[0].append(int(line))
-        for line in board_lines[1].strip(''):
-            board[1].append(int(line))
-        for line in board_lines[2].strip():
-            board[2].append(line)
+        with open('game2.frog', 'r') as file:
+            board_lines = file.readlines()
+            line = board_lines[0].split()
+            lining = list(int(num) for num in line)
+            board.append(lining)
+
+            line1 = board_lines[1].split()
+            lining1 = list(int(num) for num in line1)
+            board.append(lining1)
+            board.append([])
+
+            new_row = []
+            for i in range(2, len(board_lines)):  
+                row = list(board_lines[i].strip())
+                new_row.append(row)
+
+            board.append(new_row)
     elif file_selection == '3' or file_selection == 'game3.frog':
-        with open('game3.frog', 'r') as lines:
-            board_lines = lines.readlines()
-        for line in board_lines[0].strip(''):
-            board[0].append(int(line))
-        for line in board_lines[1].strip(''):
-            board[1].append(int(line))
-        for line in board_lines[2].strip():
-            board[2].append(line)
+        with open('game3.frog', 'r') as file:
+            board_lines = file.readlines()
+            line = board_lines[0].split()
+            lining = list(int(num) for num in line)
+            board.append(lining)
+
+            line1 = board_lines[1].split()
+            lining1 = list(int(num) for num in line1)
+            board.append(lining1)
+            board.append([])
+            
+            new_row = []
+            for i in range(2, len(board_lines)):  
+                board_row = list(board_lines[i].strip())
+                new_row.append(board_row)
+
+            board.append(new_row)
     else:
-        print("Not a selection")
-        return None
+        return "Not a selection"
 
     return board
 
 
 if __name__ == '__main__':
-    print(select_game_file())
-    # print(frogger_game(selected_game_file))
+    select = select_game_file()
+    frogger_game(select)
+    #print(frogger_game(select))
